84da86032ff5a4a3b194e4cace029798050d9669
vorobeyka <vorobeyka3222@gmail.com>
2021-01-13 04:32:00 +0200

    start work with Task_3

---
+++ Task_3/INote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    interface INote
+    {
+        int Id { get; }
+        string Title { get; }
+        string Text { get; }
+        DateTime CreatedOn { get; }
+    }
+}
